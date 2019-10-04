a41fe9eea1e0fb3f77b5c8d1985fe3db20453e17
manhpp <phumanh1998@gmail.com>
2019-10-05 01:33:50 +0700

    .

--- mip/mip.py
+++ mip/mip.py
@@ -1,22 +1,26 @@
 from ortools.linear_solver import pywraplp
 from utils.arg_parser import parse_config
-from DS.position import distance
+from prepare_data import prepare
+
 
 def solve(inp, is_adj_matrix, distance_matrix, dict_constant):
     solver = pywraplp.Solver('wsn',
                              pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
 
     num_all_vertex = len(inp.all_vertex)
-
+    available_edges = []
     connect_matrix = {}
     muy = {}
     delta = {}
     gamma = {}
     phi = {}
+    z = {}
+    i_matrix = {}
+    j_matrix = {}
 
-    a = [0 for i in range(num_all_vertex)]
-    b = [0 for i in range(num_all_vertex)]
-    e = [0 for i in range(num_all_vertex)]
+    a = [0 for _ in range(num_all_vertex)]
+    b = [0 for _ in range(num_all_vertex)]
+    e = [0 for _ in range(num_all_vertex)]
 
     for i in range(num_all_vertex):
         a[i] = solver.BoolVar('a[%i]' % i)
@@ -29,11 +33,40 @@ def solve(inp, is_adj_matrix, distance_matrix, dict_constant):
             gamma[i, j] = solver.IntVar(0, inp.num_of_sensors, 'gamma[%i,%i]' % (i, j))
             phi[i, j] = solver.BoolVar('phi[%i,%i]' % (i, j))
 
+    for i in range(num_all_vertex):
+        for j in range(1, num_all_vertex):
+            if is_adj_matrix[i][j] == 1:
+                available_edges.append((i, j))
+                for q in range(inp.max_hop + 1):
+                    for k in(1, num_all_vertex):
+                        z[i, j, q, k] = solver.BoolVar('z[%i,%i,%i,%i]' % (i, j, q, k))
+    for k in range(1, num_all_vertex):
+        for q in range(inp.max_hop + 1):
+            z[k, k, q, k] = solver.BoolVar('z[%i,%i,%i,%i]' % (k, k, q, k))
+
+    for i in range(num_all_vertex):
+        tmp = []
+        for j in range(num_all_vertex):
+            if is_adj_matrix[i][j] == 1:
+                tmp.append(j)
+        j_matrix[i] = tmp
+
+    for j in range(num_all_vertex):
+        tmp = []
+        for i in range(num_all_vertex):
+            if is_adj_matrix[i][j] == 1:
+                tmp.append(j)
+        i_matrix[j] = tmp
+
     # Objective
     solver.Minimize(
         solver.Sum(muy[i, j] * (dict_constant['E_TX'] + dict_constant['epsilon_fs'] * distance_matrix[i, j] ** 2)
                    for i in range(1, num_all_vertex) for j in range(1, num_all_vertex)) +
-        solver.Sum())
+        solver.Sum(delta[i, j] * (dict_constant['epsilon_mp'] * distance_matrix[i, j] ** 4)
+                   for i in range(1, num_all_vertex) for j in range(1, num_all_vertex)) +
+        solver.Sum(gamma[i, j] * (dict_constant['E_RX'] + dict_constant['E_DA'])
+                   for i in range(1, num_all_vertex) for j in range(1, num_all_vertex))
+    )
 
     # Constraints
     # r2
@@ -82,6 +115,40 @@ def solve(inp, is_adj_matrix, distance_matrix, dict_constant):
             solver.Add(phi[i, j] <= e[i])
             solver.Add(phi[i, j] <= connect_matrix[i, j])
             solver.Add(phi[i, j] >= e[i] + connect_matrix[i, j] - 1)
+
     for j in range(num_all_vertex):
         solver.Add(solver.Sum(phi[i, j] for i in range(num_all_vertex)) == 1)
 
+    # hop constrained
+    for i in j_matrix[0]:
+        for k in range(1, num_all_vertex):
+            solver.Add(z[0, i, 1, k] - solver.Sum(z[i, j, 2, k] for j in j_matrix[i]))
+
+    for i, j in available_edges:
+        for k in range(1, num_all_vertex):
+            solver.Add(solver.Sum(z[i, j, q, k] for q in range(1, inp.max_hop)) <= connect_matrix[i, j])
+
+    for k in range(1, num_all_vertex):
+        solver.Add(solver.Sum(z[j, k, inp.max_hop, k] for j in i_matrix[k]) + z[k, k, inp.max_hop, k] == 1)
+
+    for k in range(1, num_all_vertex):
+        for q in range(2, inp.max_hop-1):
+            solver.Add(
+                solver.Sum(z[j, i, q, k] for j in i_matrix[i]) - solver.Sum(z[i, j, q+1, k] for j in j_matrix[i]) == 0)
+
+    print('Number of constraints =', solver.NumConstraints())
+    solver.Solve()
+    print('optimal value = ', solver.Objective().Value())
+    print()
+    for i in range(num_all_vertex):
+        for j in range(num_all_vertex):
+            print(connect_matrix[i, j])
+
+    print()
+    print("Time = ", solver.WallTime(), " milliseconds")
+
+
+if __name__ == '__main__':
+    _inp, _is_adj_matrix, _distance_matrix = prepare('/home/manhpp/Documents/Code/WSN/data/ga-dem1_r25_1.in')
+    _dict_constant = parse_config()
+    solve(_inp, _is_adj_matrix, _distance_matrix, _dict_constant)
--- mip/prepare_data.py
+++ mip/prepare_data.py
@@ -6,17 +6,20 @@ from DS.position import distance
 def prepare(path):
     inp = WsnInput.from_file(path)
     is_adj_matrix = [[0 for _ in range(len(inp.all_vertex))] for _ in range(len(inp.all_vertex))]
-
+    distance_matrix = {}
     for i in inp.all_vertex:
         for j in inp.all_vertex:
             if j.name == 0:
+                distance_matrix[i.name, j.name] = float('inf')
                 continue
             if i.name > inp.num_of_relay_positions >= j.name:
+                distance_matrix[i.name, j.name] = float('inf')
                 continue
             if distance(i, j) <= inp.radius and distance(i, j) != 0:
+                distance_matrix[i.name, j.name] = distance(i, j)
                 is_adj_matrix[i.name][j.name] = 1
 
-    return inp, is_adj_matrix
+    return inp, is_adj_matrix, distance_matrix
 
 
 if __name__ == '__main__':
