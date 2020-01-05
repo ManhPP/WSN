ccc873e8c66f11f113ba0d754365353234da4437
manhpp <phumanh1998@gmail.com>
2020-01-05 21:31:29 +0700

    .

--- utils/init_log.py
+++ utils/init_log.py
@@ -1,9 +1,12 @@
 import logging
 import logging.config
+from pathlib import Path
 
 
 def init_log():
-    logging.config.fileConfig('..\\utils\\logging.conf')
+    config_file_path = Path(__file__).parent
+
+    logging.config.fileConfig(f"{config_file_path}/logging.conf")
     logger = logging.getLogger(__name__)
     logger.info("Custom logging started.")
     logger.info("Complete!")
