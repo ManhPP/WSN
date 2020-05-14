9d891eee044be7c91c19f101d5aabe9d24064d4f
manhpp <phumanh1998@gmail.com>
2020-05-15 00:13:43 +0700

    .

--- DS/edge.py
+++ DS/edge.py
@@ -4,12 +4,8 @@ from DS.position import distance
 
 class Edge:
     def __init__(self, v1: Vertex = None, v2: Vertex = None):
-        self.vertices = sorted([v1, v2], key=self.get_vertex_name)
+        self.vertices = sorted([v1, v2], key=lambda x: x.name)
         self._distance = distance(v1,v2)
-
-    @staticmethod
-    def get_vertex_name(v: Vertex):
-        return v.name
 
     @property
     def distance(self):
--- src/draft.py
+++ src/draft.py
@@ -1,4 +1,5 @@
 from collections import deque
+import random
 
 from DS.graph import Graph
 from utils.arg_parser import parse_config
@@ -8,25 +9,32 @@ from utils.load_input import WsnInput
 inf = float('inf')
 
 
-if __name__ == '__main__':
-    logger = init_log()
-    path = 'D:\\Code\\WSN\\data\\test.json'
-    params, _ = parse_config()
-    logger.info("prepare input data from path %s" % path)
-    inp = WsnInput.from_file(path)
+def random_choices(seq, num):
+    result = []
+    while len(result) != num:
+        choice = random.choice(seq)
+        if choice not in result:
+            result.append(choice)
+    return result
 
+
+def spt(path=None):
+    if not path:
+        _, path = parse_config()
+
+    inp = WsnInput.from_file(path)
     vertices = inp.all_vertex[:]
-    dict_ind2edge = inp.dict_ind2edge
 
     graph = Graph()
 
-    position_relay_index = [1, 2, 3]
+    position_relay_indices = random_choices(range(1, inp.num_of_relay_positions + 1), inp.num_of_relays)
+
     v0 = inp.all_vertex[0]
     for i in inp.dict_ind2edge.keys():
         edge = inp.dict_ind2edge[i]
         v1, v2 = edge.vertices
         if v1.name == v0.name:
-            if v2.name in position_relay_index:
+            if v2.name in position_relay_indices:
                 graph.add_edge(edge)
 
     distances = {vertex: inf for vertex in vertices}
@@ -40,13 +48,52 @@ if __name__ == '__main__':
         vertices.remove(current_vertex)
         if distances[current_vertex] == inf:
             break
+        if current_vertex.type_of_vertex == 'relay' and current_vertex.name not in position_relay_indices:
+            continue
 
         for adj in current_vertex.adjacent_vertices:
+            if adj.type_of_vertex == 'relay':
+                if current_vertex.type_of_vertex == "bs" or adj.name not in position_relay_indices:
+                    continue
             distance = adj.get_distance(current_vertex)
             new_distance = distances[current_vertex] + distance
             if new_distance < distances[adj]:
                 distances[adj] = new_distance
                 previous_vertices[adj] = current_vertex
+    list_edge = []
+    for item in previous_vertices.items():
+        if None not in item:
+            list_edge.append(sorted(item, key=lambda x: x.name))
 
-    path = deque()
+    edge_indices = []
+    for item in inp.dict_ind2edge.items():
+        if item[1].vertices in list_edge:
+            graph.add_edge(item[1])
+            edge_indices.append(item[0])
 
+    return graph, edge_indices, position_relay_indices
+
+
+def mst(path=None):
+    if not path:
+        _, path = parse_config()
+
+    inp = WsnInput.from_file(path)
+    vertices = inp.all_vertex[:]
+
+    graph = Graph()
+
+    position_relay_indices = random_choices(range(1, inp.num_of_relay_positions + 1), inp.num_of_relays)
+
+    v0 = inp.all_vertex[0]
+    for i in inp.dict_ind2edge.keys():
+        edge = inp.dict_ind2edge[i]
+        v1, v2 = edge.vertices
+        if v1.name == v0.name:
+            if v2.name in position_relay_indices:
+                graph.add_edge(edge)
+
+
+if __name__ == '__main__':
+    g, e, p = spt()
+    tmp = deque()
