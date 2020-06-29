6a427af746b9cdba21605fe166e4dc119a62d694
manhpp <phumanh1998@gmail.com>
2020-06-30 00:30:48 +0700

    .

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# WSN-
+# WSN
+##data path: 
+`data_path` in `utils/config.ini`
+## run MIP:
+run `integer_programming/mip_v3.py`
+## run CP-SAT:
+run `integer_programming/mip_cp_sat.py`
+##run GA:
+run `src/GA.py`
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,8 +1,8 @@
 #gauss
-python ./scripts/hop_datagen.py -o data -W 200 -H 200 --depth 1 --height 10 --rows 41 --cols 41 --num-sensor 20 --num-relay 10 --num-relay-position 20 --max-hop 5 --csize 5 --radius 40 --prefix no- --distribution gaussian data/dems_data/*.asc
+python ./scripts/hop_datagen.py -o data -W 200 -H 200 --depth 1 --height 10 --rows 41 --cols 41 --num-sensor 20 --num-relay 10 --num-relay-position 20 --max-hop 5 --csize 5 --radius 25 --prefix no- --distribution gaussian data/dems_data/*.asc
 #gamma
-python ./scripts/hop_datagen.py -o data -W 200 -H 200 --depth 1 --height 10 --rows 41 --cols 41 --num-sensor 20 --num-relay 10 --num-relay-position 20 --max-hop 5 --csize 5 --radius 40 --prefix ga- --distribution gamma data/dems_data/*.asc
+python ./scripts/hop_datagen.py -o data -W 200 -H 200 --depth 1 --height 10 --rows 41 --cols 41 --num-sensor 20 --num-relay 10 --num-relay-position 20 --max-hop 5 --csize 5 --radius 25 --prefix ga- --distribution gamma data/dems_data/*.asc
 #uniform
-python ./scripts/hop_datagen.py -o data -W 200 -H 200 --depth 1 --height 10 --rows 41 --cols 41 --num-sensor 20 --num-relay 10 --num-relay-position 20 --max-hop 5 --csize 5 --radius 40 --prefix uu- --distribution uniform data/dems_data/*.asc
+python ./scripts/hop_datagen.py -o data -W 200 -H 200 --depth 1 --height 10 --rows 41 --cols 41 --num-sensor 20 --num-relay 10 --num-relay-position 20 --max-hop 5 --csize 5 --radius 25 --prefix uu- --distribution uniform data/dems_data/*.asc
 
 sleep 5s
