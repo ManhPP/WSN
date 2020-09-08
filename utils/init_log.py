d9532b6f162365a55f99c9f00220972bfe52c07f
manhpp <phumanh1998@gmail.com>
2020-09-08 15:08:31 +0700

    .

--- utils/init_log.py
+++ utils/init_log.py
@@ -7,8 +7,6 @@ from pathlib import Path
 def init_log():
     config_file_path = Path(__file__).parent
     path = f"{config_file_path.parent}/logs"
-    print("log file: ", path)
-    print(config_file_path)
     if not os.path.exists(path):
         os.makedirs(path)
     logging.config.fileConfig(f"{config_file_path}/logging.conf")
