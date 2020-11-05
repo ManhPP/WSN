a28620ae1bbd470f69ae8600493c0022137f9c24
manhpp <phumanh1998@gmail.com>
2020-11-05 15:34:13 +0700

    revert log

--- utils/init_log.py
+++ utils/init_log.py
@@ -6,10 +6,10 @@ from pathlib import Path
 
 def init_log():
     config_file_path = Path(__file__).parent
-    path = "%s/logs"%config_file_path.parent
+    path = f"{config_file_path.parent}/logs"
     if not os.path.exists(path):
         os.makedirs(path)
-    logging.config.fileConfig("%s/logging.conf"%config_file_path)
+    logging.config.fileConfig(f"{config_file_path}/logging.conf")
     logger = logging.getLogger(__name__)
     logger.info("Logger started.")
     return logger
