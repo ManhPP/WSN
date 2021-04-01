3edc8f3a180fe4a6bc5026b848d27278ed8c5919
manhpp <phumanh1998@gmail.com>
2021-04-01 13:06:10 +0700

    update cp sat

--- integer_programming/mip_cp_sat.py
+++ integer_programming/mip_cp_sat.py
@@ -159,6 +159,7 @@ def solve_by_or_tools(logger, inp, is_adj_matrix, distance_matrix, dict_constant
 
     # print('Number of constraints =', model.NumConstraints())
     solver = cp_model.CpSolver()
+    solver.parameters.max_time_in_seconds = 30
     # cp_model.sat_parameters_pb2
     solver.parameters.num_search_workers = 4
     solver.parameters.log_search_progress = True
